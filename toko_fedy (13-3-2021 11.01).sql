4bb266a8b81704b9ace2852077dfbdef55b0127f
fedyrahmatullah <48233176+fedyrahmatullah@users.noreply.github.com>
2021-08-31 18:57:21 +0700

    Initial commit

---
+++ toko_fedy (13-3-2021 11.01).sql
@@ -0,0 +1,269 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 13 Mar 2021 pada 05.01
+-- Versi server: 10.4.8-MariaDB
+-- Versi PHP: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `toko_fedy`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `username` varchar(35) NOT NULL,
+  `password` varchar(35) NOT NULL,
+  `no_tlp` varchar(14) NOT NULL,
+  `level` enum('admin','karyawan') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `username`, `password`, `no_tlp`, `level`) VALUES
+(1, 'fedy', 'd245411c5701fea25091bf9222079a87', '081335555', 'admin'),
+(2, 'kasir', 'c7911af3adbd12a035b289556d96470a', '08676372827', 'karyawan');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `kode_barang` varchar(32) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `id_kategori` varchar(50) NOT NULL,
+  `harga_jual` int(50) NOT NULL,
+  `stok` int(50) NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`kode_barang`, `nama_barang`, `id_kategori`, `harga_jual`, `stok`, `keterangan`) VALUES
+('RJ0001', 'Sabun Mandi', 'KB0001', 5000, 10, 'Sabun Mandi Lifebuoy Lemon');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buku_besar`
+--
+
+CREATE TABLE `buku_besar` (
+  `id_bukubesar` int(11) NOT NULL,
+  `kode_transaksi` varchar(12) NOT NULL,
+  `tipe` enum('penjualan','pembelian','kas') NOT NULL,
+  `tanggal` date NOT NULL,
+  `nominal` int(10) NOT NULL,
+  `jenis` enum('debit','kredit') NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_pembelian`
+--
+
+CREATE TABLE `detail_pembelian` (
+  `id_detail` int(5) NOT NULL,
+  `kode_pembelian` varchar(50) NOT NULL,
+  `kode_barang` varchar(50) NOT NULL,
+  `qty` int(50) NOT NULL,
+  `harga_satuan` int(50) NOT NULL,
+  `keterangan` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_penjualan`
+--
+
+CREATE TABLE `detail_penjualan` (
+  `id_detail` int(50) NOT NULL,
+  `kode_penjualan` varchar(50) NOT NULL,
+  `kode_barang` varchar(50) NOT NULL,
+  `qty` int(50) NOT NULL,
+  `harga_satuan` int(50) NOT NULL,
+  `keterangan` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kas`
+--
+
+CREATE TABLE `kas` (
+  `kode_kas` varchar(9) NOT NULL,
+  `tanggal_kas` date NOT NULL,
+  `nominal` int(10) NOT NULL,
+  `jenis` enum('debit','kredit') NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` varchar(50) NOT NULL,
+  `nama_kategori` varchar(59) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
+('KB0001', 'Alat Rumah Tangga'),
+('KB0002', 'Bahan Pokok');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pembelian`
+--
+
+CREATE TABLE `pembelian` (
+  `kode_pembelian` varchar(50) NOT NULL,
+  `tanggal_pembelian` varchar(50) NOT NULL,
+  `total` int(50) NOT NULL,
+  `id_admin` int(50) NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `kode_penjualan` varchar(30) NOT NULL,
+  `tanggal_penjualan` varchar(35) NOT NULL,
+  `nama_pembeli` varchar(35) NOT NULL,
+  `total_qty` int(35) NOT NULL,
+  `total_penjualan` int(50) NOT NULL,
+  `total_bayar` int(50) NOT NULL,
+  `potongan` int(11) NOT NULL,
+  `id_admin` int(35) NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`kode_barang`);
+
+--
+-- Indeks untuk tabel `buku_besar`
+--
+ALTER TABLE `buku_besar`
+  ADD PRIMARY KEY (`id_bukubesar`);
+
+--
+-- Indeks untuk tabel `detail_pembelian`
+--
+ALTER TABLE `detail_pembelian`
+  ADD PRIMARY KEY (`id_detail`);
+
+--
+-- Indeks untuk tabel `detail_penjualan`
+--
+ALTER TABLE `detail_penjualan`
+  ADD PRIMARY KEY (`id_detail`);
+
+--
+-- Indeks untuk tabel `kas`
+--
+ALTER TABLE `kas`
+  ADD PRIMARY KEY (`kode_kas`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indeks untuk tabel `pembelian`
+--
+ALTER TABLE `pembelian`
+  ADD PRIMARY KEY (`kode_pembelian`);
+
+--
+-- Indeks untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`kode_penjualan`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `buku_besar`
+--
+ALTER TABLE `buku_besar`
+  MODIFY `id_bukubesar` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `detail_pembelian`
+--
+ALTER TABLE `detail_pembelian`
+  MODIFY `id_detail` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `detail_penjualan`
+--
+ALTER TABLE `detail_penjualan`
+  MODIFY `id_detail` int(50) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
